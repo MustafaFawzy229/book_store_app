58b2e12cebfdd339c31a0a8821476853f3ad19c8
Mustafa Fawzy <hamouda777744@gmail.com>
2023-06-29 16:33:25 +0300

    change app name

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:get/get_navigation/get_navigation.dart';
 import 'Features/Splash/presentation/views/splash_view.dart';
 
 void main() {
-  runApp(const BookStoreApp());
+  runApp(const Bookly());
 }
 
-class BookStoreApp extends StatelessWidget {
-  const BookStoreApp({super.key});
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
 
   @override
   Widget build(BuildContext context) {
